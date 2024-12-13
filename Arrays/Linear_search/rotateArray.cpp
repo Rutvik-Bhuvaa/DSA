f2f11e5236e8b779c224676729d0afd68041a23b
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2024-12-13 12:16:30 +0530

    Rotate array by one added in linear search folder

--- Arrays/Linear_search/rotateArray.cpp
+++ Arrays/Linear_search/rotateArray.cpp
@@ -5,6 +5,16 @@ using namespace std;
 
 int main(){
     int arr[8] = {2, 3, 7, -11, 1, 8, 13, 12};
-    
+    int lastNumber = arr[7];
+
+    for(int i=6; i>=0; i--){
+        arr[i+1] = arr[i];
+    }
+
+    arr[0] = lastNumber;
+
+    for(int i=0; i<8; i++){
+        cout << arr[i] << " "; 
+    }
     return 0;
 }
